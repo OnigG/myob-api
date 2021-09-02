69bb839582fd4a6f82b0e4f120f5fa2725988eaf
OnigG <88769190+OnigG@users.noreply.github.com>
2021-09-02 14:42:07 +0800

    Update company.rb

--- lib/myob/api/models/company.rb
+++ lib/myob/api/models/company.rb
@@ -3,6 +3,16 @@ module Myob
     module Model
       class Company < Base
       end
+      class Preferences < Base
+            def model_route
+              'Company/Preferences'
+            end
+      end
+      class FormTemplate < Base
+            def model_route
+              'Company/FormTemplate'
+            end
+      end
     end
   end
 end
