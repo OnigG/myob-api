6034079d37511f5b68d5466de822975542ab30df
OnigG <88769190+OnigG@users.noreply.github.com>
2021-09-02 15:39:57 +0800

    Add files via upload

---
+++ lib/myob/api/models/report.rb
@@ -0,0 +1,48 @@
+module Myob
+    module Api
+      module Model
+        class Report < Base
+          def model_route
+            'Report'
+          end
+        end
+  
+        class ReportPayrollEmployeePayrollAdvice < Base
+          def model_route
+            'Report/Payroll/EmployeePayrollAdvice'
+          end
+        end
+  
+        class ReportGSTNZGSTReport < Base
+          def model_route
+            'Report/GST/NZGSTReport'
+          end
+        end
+  
+        class ReportTaxCodeSummary < Base
+          def model_route
+            'Report/TaxCodeSummary'
+          end
+        end
+  
+        class ReportProfitAndLossSummary < Base
+          def model_route
+            'Report/ProfitAndLossSummary'
+          end
+        end
+  
+        class ReportPayrollCategorySummary < Base
+          def model_route
+            'Report/PayrollCategorySummary'
+          end
+        end
+  
+        class ReportTransactionCodingSummary < Base
+          def model_route
+            'Report/TransactionCodingSummary'
+          end
+        end
+      end
+    end
+  end
+  
