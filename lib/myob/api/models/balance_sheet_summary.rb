bc33b44492ce9d3a0699aef546340d6c03a45d98
OnigG <88769190+OnigG@users.noreply.github.com>
2021-09-06 16:25:33 +0800

    Update balance_sheet_summary.rb

--- lib/myob/api/models/balance_sheet_summary.rb
+++ lib/myob/api/models/balance_sheet_summary.rb
@@ -3,8 +3,15 @@ module Myob
     module Model
         class BalanceSheetSummary < Base
           def model_route
-            'Report/BalanceSheetSummary?StartDate=2018-01-01&EndDate=2018-05-31&ReportingBasis=Accrual&YearEndAdjust=true'
-          end 
+            'Report/BalanceSheetSummary'
+          end
+          
+          def select(AoD,YEA)
+            object = "AsOfDate="+ AoD.to_s 
+            object2 =  "YearEndAdjust=" + YEA.to_s
+            perform_request(self.url?(object&object2))
+          end
+          
         end
     end
   end
