65aa400eea0c5cbb31bccdb5de5b338e03084471
OnigG <88769190+OnigG@users.noreply.github.com>
2021-09-06 15:13:54 +0800

    Update balance_sheet_summary.rb

--- lib/myob/api/models/balance_sheet_summary.rb
+++ lib/myob/api/models/balance_sheet_summary.rb
@@ -4,24 +4,8 @@ module Myob
         class BalanceSheetSummary < Base
           def model_route
             'Report/BalanceSheetSummary'
-          end      
-          
-        def new_record?(object)
-          false
+          end     
         end
-
-        def for(as_of_date, year_end_adjust)
-          self.send(:perform_request, url({ 'AsOfDate' => as_of_date, 'YearEndAdjust' => year_end_adjust}))
-        end
-
-        def url(object = nil, params = nil)
-          if object
-            "#{super()}/#{object?AsOfDate=#{object['AsOfDate']}&YearEndAdjust=#{object['YearEndAdjust']}"
-          else
-            super
-          end
-        end
-      end
     end
   end
 end
